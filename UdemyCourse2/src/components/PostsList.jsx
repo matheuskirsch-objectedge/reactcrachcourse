733ab2e848c5f4bde34bd1e8b08fa05bd075238a
Matheus <matheus.kirsch@objectedge.com>
2025-09-19 11:53:39 -0300

    third commit

--- UdemyCourse2/src/components/PostsList.jsx
+++ UdemyCourse2/src/components/PostsList.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+    const [enteredBody, SetEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+
+        function bodyChangeHandler(event) {
+            SetEnteredBody(event.target.value);
+        }   
+    
+        function authorChangeHandler(event) {
+            setEnteredAuthor(event.target.value);
+        }   
+
     return (
+        <>
+        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
         <ul className={classes.posts}>
-            <Post author= "Matheus" body="React.js is aewsome!"/>
-            <Post author="Mellanie" body="Check out the full course, before you think of buying it. But i am sure uou will like it. Thanks!!!"/>
+            <Post author= {enteredAuthor} body={enteredBody}/>
+            <Post author="Mellanie" body="Check out the full course!"/>
         </ul> 
+        </>
     )
 }
 
